77fe0a2769052750505ae13540417fc6f22a52e0
William Howard-Snyder <william122899@gmail.com>
2023-02-04 17:19:43 -0800

    Abstract class for all models. Still work in progress

--- models/LinkPredModel.py
+++ models/LinkPredModel.py
@@ -8,3 +8,15 @@ class model(ABC):
     
     def __init__(self) -> None:
         super().__init__()
+
+
+    def train(graph) -> None:
+        """
+        Given a graph as a list of edges, initialize appropriate model parameters so that we can
+        quickly determien whether there is an edge between two nodes 
+        """
+
+    def score(node1, node2) -> float:
+        """
+        Return a score indicating whether there should be an edge between the two nodes
+        """

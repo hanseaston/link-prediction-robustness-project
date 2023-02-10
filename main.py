e7779d3ae601e638f8ef71f75c2cecb319cdc1d0
Hans Easton <hanreaston@gmail.com>
2023-02-09 17:43:56 -0800

    some more updates

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from ogb.linkproppred import LinkPropPredDataset
+import gzip
+import shutil
+from ogb.linkproppred import PygLinkPropPredDataset
 
-dataset = LinkPropPredDataset(name = "ogbl-ddi", root = 'dataset/')
 
-split_edges = dataset.get_edge_split()
+def setup():
+    # downloads the dataset if there's no local copy
+    PygLinkPropPredDataset(name="ogbl-ddi", root='./dataset/')
+    # unzips all the raw data
+    for file_path in ["dataset/ogbl_ddi/raw/edge.csv", \
+                    "dataset/ogbl_ddi/raw/num-edge-list.csv", \
+                    "dataset/ogbl_ddi/raw/num-node-list.csv"]:
+        with gzip.open(file_path + ".gz", 'rb') as f_in:
+            with open(file_path, 'wb') as f_out:
+                shutil.copyfileobj(f_in, f_out)
 
-training_dataset = split_edges["train"]["edge"]
-validation_dataset = split_edges["valid"]["edge"]
-testing_dataet = split_edges["test"]["edge"]
-
-# Do whatever you want! 
+# sets up the dataset on your local computer
+if __name__ == "__main__":
+    setup()
--- models/Neighborhood.py
+++ models/Neighborhood.py
@@ -2,31 +2,36 @@ from models.LinkPredModel import LinkPredictor
 import networkx as nx
 
 
-class CommonNeighbors(LinkPredictor):
+class JaccardSimilarity(LinkPredictor):
 
     def __init__(self) -> None:
         super().__init__()
 
     def train(self, graph: nx.Graph, **kwargs:dict) -> None:
-        nodes = graph.number_of_nodes()
-        print("how many nodes", nodes)
-    
-        for i in range(nodes):
-            # need the community information for the algorithm to work, in this case, set it all to 0
-            graph.nodes[i]["community"]=  0
-        predictions = nx.cn_soundarajan_hopcroft(G=graph,ebunch=None)
+
+        # calculating the jaccard similarity of all the nodes that do not have an existing edge
+        jaccard_similarity = nx.jaccard_coefficient(graph, [(4039, 2424)])
+
         self.predictions = {}
-        for u, v, p in predictions:
-            self.predictions[(u, v)] = p
+        for u, v, p in jaccard_similarity:
+            smaller = min(u, v)
+            larger = max(u, v)
+            self.predictions[(smaller, larger)] = p
 
 
     def score_edge(self, node1:int, node2:int) -> float:
-        return self.predictions[(node1, node2)]
+        # ensures symmetric ordering
+        smaller = min(node1, node2)
+        larger = max(node1, node2)
+        key = (smaller, larger)
+        if key not in self.predictions:
+            raise Exception("the nodes are found in graph")
+        return self.predictions[key]
     
     def score_edges(self, edge_list:list):
         scores = []
         for edge in edge_list:
-            scores.append(self.predictions(edge.node1, edge.node2))
+            scores.append(self.score_edge(edge))
 
     def save_model(self, model_path=None):
         pass

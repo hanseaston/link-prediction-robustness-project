f2df2e9d69ac06ea1f3af7455e157da88b4d4b34
Therese Pacio <tpacio@cs.washington.edu>
2023-02-11 16:20:59 -0800

    fixed training latentfactor

--- models/LatentFactor.py
+++ models/LatentFactor.py
@@ -15,8 +15,9 @@ class MatrixFactorization(LinkPredictor):
 
     def __init__(self) -> None:
         super().__init__()
-        self.Q = None
-        self.P_T = None
+        self.eigenvect = None
+        self.eigenval = None
+        self.adj_mat = None
         
 
     def train(self, graph:list, **kwargs:dict) -> None:
@@ -24,15 +25,21 @@ class MatrixFactorization(LinkPredictor):
         eigenval, eigenvec = np.linalg.eig(adj_matrix.toarray())
         eigenval = np.real(eigenval)
         eigenvect = np.real(eigenvec)
-        self.Q = eigenval
-        self.P_T = eigenvect
+        self.eigenvect = eigenvect
+        self.eigenval = eigenval
+        self.adj_mat = adj_matrix
 
     
     def score_edge(self, node1:int, node2:int) -> float:
         """
         calculating score using dotproduct of Q_r and P_T_i as shown in slide 20 of rec_sys2 lecture
         """
-        return np.dot(self.Q[node1], self.P_T[node2])
+        # get the k = 100 projection 
+        k = 500
+        proj_matrix = self.eigenvect[:, :k]
+        n1_proj = self.adj_mat[node1].T @ proj_matrix
+        n2_proj = self.adj_mat[node2].T @ proj_matrix
+        return np.dot(n1_proj, n2_proj)
 
     def score_edges(self, edge_list:list) -> np.ndarray:
         res = np.zeros(len(edge_list))
@@ -43,9 +50,12 @@ class MatrixFactorization(LinkPredictor):
 
 
     def save_model(self, model_path=None):
-        np.savez(model_path, self.Q, self.P_T)
+        import ipdb
+        ipdb.set_trace()
+        np.savez(model_path, self.eigenval, self.eigenvect, self.adj_mat.toarray())
     
     def load_model(self, model_path=None):
         npz = np.load(model_path)
-        self.Q = npz['arr_0']
-        self.P_T = npz['arr_0']
+        self.eigenval = npz['arr_0']
+        self.eigenvect = npz['arr_1']
+        self.adj_mat = npz['arr_2']

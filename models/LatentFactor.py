f9cec3debb7fb26a72dc50c05bb6ef4d75fd5f57
Therese Pacio <tpacio@cs.washington.edu>
2023-02-12 12:24:53 -0800

    swapped dot product for cosin distance

--- models/LatentFactor.py
+++ models/LatentFactor.py
@@ -1,6 +1,7 @@
 from models.LinkPredModel import LinkPredictor
 import networkx as nx
 import numpy as np
+from scipy.spatial import distance
 
 
 class MatrixFactorization(LinkPredictor):
@@ -25,21 +26,23 @@ class MatrixFactorization(LinkPredictor):
         eigenval, eigenvec = np.linalg.eig(adj_matrix.toarray())
         eigenval = np.real(eigenval)
         eigenvect = np.real(eigenvec)
-        self.eigenvect = eigenvect
-        self.eigenval = eigenval
+        # save first 100 eigenvectors and eigenvalues 
+        k = 100
+        self.eigenvect = eigenvect[:, :k]
+        self.eigenval = eigenval[:k]
         self.adj_mat = adj_matrix
 
     
     def score_edge(self, node1:int, node2:int) -> float:
         """
         calculating score using dotproduct of Q_r and P_T_i as shown in slide 20 of rec_sys2 lecture
+        notes : reconstruction performs the same as the projection
         """
-        # get the k = 100 projection 
-        k = 500
-        proj_matrix = self.eigenvect[:, :k]
-        n1_proj = self.adj_mat[node1].T @ proj_matrix
-        n2_proj = self.adj_mat[node2].T @ proj_matrix
-        return np.dot(n1_proj, n2_proj)
+        n1_proj = self.adj_mat[node1].T @ self.eigenvect
+        n2_proj = self.adj_mat[node2].T @ self.eigenvect
+        n1_recon = n1_proj @ self.eigenvect.T
+        n2_recon = n2_proj @ self.eigenvect.T
+        return distance.cosine(n1_proj, n2_proj)
 
     def score_edges(self, edge_list:list) -> np.ndarray:
         res = np.zeros(len(edge_list))
@@ -50,8 +53,6 @@ class MatrixFactorization(LinkPredictor):
 
 
     def save_model(self, model_path=None):
-        import ipdb
-        ipdb.set_trace()
         np.savez(model_path, self.eigenval, self.eigenvect, self.adj_mat.toarray())
     
     def load_model(self, model_path=None):

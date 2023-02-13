c39b000ded32e81d6a7dbeb9e1eb10b98469e33f
Hans Easton <hanreaston@gmail.com>
2023-02-12 23:12:34 -0800

    implement common neighbor

---
+++ experiments/test_commonneighbor.py
@@ -0,0 +1,112 @@
+import sys
+import os
+sys.path.append(os.getcwd())
+#####################################################################
+
+from ogb.linkproppred import PygLinkPropPredDataset, Evaluator
+from models.Neighborhood import CommonNeighbor
+import pandas as pd
+import numpy as np
+import networkx as nx
+import time
+
+TRAIN = False
+
+"""
+Sandbox for Common Neighbor algorithm link prediction
+"""
+
+def main():
+    start = time.time()
+
+    if TRAIN:
+        train_test()
+    else:
+        load_test()
+
+    end = time.time()
+
+    print(f"Script took {round((end - start) / 60, 2)} minutes to run")
+
+
+def train_test():
+    """
+    Trains and saves model using abstract class methods
+    """
+    print("=> Preparing dataset...")
+    PygLinkPropPredDataset(name="ogbl-ddi", root='./dataset/')
+    df = pd.read_csv("dataset/ogbl_ddi/raw/edge.csv", names=["source", "target"])
+    G = nx.from_pandas_edgelist(df)
+
+    print("=> Initializing Common Neighbor model...")
+    model = CommonNeighbor()
+
+    print("=> Training model...")
+    model.train(G)
+
+    print("=> Saving model...")
+    model.save_model()
+
+
+
+def load_test():
+    """
+    Loads and tests model using abstract class methods
+    """
+    print("=> Preparing dataset...")
+    dataset = PygLinkPropPredDataset(name="ogbl-ddi", root='./dataset/')
+
+    split_edge = dataset.get_edge_split()
+    df = pd.read_csv("dataset/ogbl_ddi/raw/edge.csv", names=["source", "target"])
+    G = nx.from_pandas_edgelist(df)
+
+    print("=> Initializing Common Neighbor model...")
+    model = CommonNeighbor()
+
+    print("=> Loading model...")
+    model.load_model() 
+    
+    print("=> Testing model...")
+
+    pos_valid_preds = model.score_edges(split_edge["valid"]["edge"].tolist())
+    neg_valid_preds = model.score_edges(split_edge["valid"]["edge_neg"].tolist())
+
+    pos_test_pred = model.score_edges(split_edge["test"]["edge"].tolist())
+    neg_test_pred = model.score_edges(split_edge["test"]["edge_neg"].tolist())
+
+    evaluator = Evaluator(name='ogbl-ddi')
+    results = {}
+
+    # metrics on validation test
+    for K in [20, 50, 100]:
+        evaluator.K = K
+        hits = evaluator.eval({
+            'y_pred_pos': np.array(pos_valid_preds),
+            'y_pred_neg': np.array(neg_valid_preds),
+        })[f'hits@{K}']
+
+        results[f'Hits@{K}'] = hits
+    
+    with open("results/commonneighbor.txt", 'w') as f:
+        f.write("On validation set, model achieves:\n")
+        f.write(str(results) + "\n\n")
+
+    # metrics on test test
+    for K in [20, 50, 100]:
+        evaluator.K = K
+        hits = evaluator.eval({
+            'y_pred_pos': np.array(pos_test_pred),
+            'y_pred_neg': np.array(neg_test_pred),
+        })[f'hits@{K}']
+
+        results[f'Hits@{K}'] = hits
+    
+    with open('results/commonneighbor.txt', 'a') as f:
+        f.write("On test set, model achieves:\n")
+        f.write(str(results))
+
+
+
+if __name__ == "__main__":
+    main()
+
--- models/Neighborhood.py
+++ models/Neighborhood.py
@@ -42,6 +42,51 @@ class JaccardSimilarity(LinkPredictor):
         with open("pickle/jaccard.pickle", 'rb') as handle:
             self.predictions = pickle.load(handle)
 
+class CommonNeighbor(LinkPredictor):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def train(self, graph: nx.Graph, **kwargs:dict) -> None:
+
+        # For now, we are not considering the communities in the graph
+        # To get around the community restriction in nx.cn_soundarajan_hopcroft,
+        # we treat each node as a different community
+        for i in range(graph.number_of_nodes()):
+            graph.nodes[i]['community'] = i
+
+        # calculating the common neighbor metrics of all the nodes that do not have an existing edge
+        common_neighbors = nx.cn_soundarajan_hopcroft(graph)
+
+        self.predictions = {}
+        for u, v, p in common_neighbors:
+            smaller = min(u, v)
+            larger = max(u, v)
+            self.predictions[(smaller, larger)] = p
+
+    def score_edge(self, node1:int, node2:int) -> float:
+        # ensures consistent ordering
+        smaller = min(node1, node2)
+        larger = max(node1, node2)
+        key = (smaller, larger)
+        if key not in self.predictions:
+            raise Exception("the nodes are found in graph")
+        return self.predictions[key]
+    
+    def score_edges(self, edge_list:list):
+        scores = []
+        for node1, node2 in edge_list:
+            scores.append(self.score_edge(node1, node2))
+        return scores
+
+    def save_model(self, model_path=None):
+        with open("pickle/commonneighbor.pickle", 'wb') as handle:
+            pickle.dump(self.predictions, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    
+    def load_model(self, model_path=None):
+        with open("pickle/commonneighbor.pickle", 'rb') as handle:
+            self.predictions = pickle.load(handle)
+
 
 class AdamicAdar(LinkPredictor):
 
@@ -49,7 +94,7 @@ class AdamicAdar(LinkPredictor):
         super().__init__()
 
     def train(self, graph:list, **kwargs:dict) -> None:
-        # calculating the jaccard similarity of all the nodes that do not have an existing edge
+        # calculating the adamic-adar index of all the nodes that do not have an existing edge
         adamic_adar_index = nx.adamic_adar_index(graph)
 
         self.predictions = {}

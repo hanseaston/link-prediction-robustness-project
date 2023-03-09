30d418856f4d0435977efac67bb2818f726bcfb5
William Howard-Snyder <william122899@gmail.com>
2023-03-08 19:01:23 -0800

    degree scatter plot

--- analysis/main.py
+++ analysis/main.py
@@ -5,9 +5,8 @@ sys.path.append(os.getcwd())
 
 import gzip
 import shutil
-from ogb.linkproppred import PygLinkPropPredDataset, Evaluator
-from models.GraphSAGE import GraphSAGE
-from models.Neighborhood import RuntimeCN
+from ogb.linkproppred import Evaluator
+
 import pandas as pd
 import numpy as np
 import networkx as nx
@@ -46,7 +45,7 @@ model_name = {
 
 
 def main():
-    props = [-0.5, -0.25, -0.1, 0.1, 0.25, 0.5, 1]
+    props = [-0.5, -0.25, -0.1, -0.01, 0, 0.01, 0.1, 0.25, 0.5, 1]
     pert_type = "random"
     models = [
         "gnn",
@@ -54,11 +53,15 @@ def main():
         "trained_mf"
     ]
     out_dir = f"results/figures/{pert_type}"
-
-    # plot_hits(props, pert_type, models, out_dir, K=20)
+    
+    # for K in [20, 50, 100]:
+    #     plot_hits(props, pert_type, models, out_dir, K=K)
 
     rank_dict, baseline = get_rankings(props, pert_type, models)
-    plot_average_edge_change(rank_dict, baseline, props, out_dir)
+
+    # plot_average_edge_change(rank_dict, baseline, props, out_dir)
+
+    error_analysis(rank_dict, baseline, 0.5, out_dir)
 
 
 def get_rankings(props, pert_type, models):
@@ -87,6 +90,9 @@ def get_rankings(props, pert_type, models):
 
         for model in models:
             score_path = f"results/scores/{pert_type}/{pert}/{print_prop}/{model}_scores.pth"
+            if not os.path.isfile(score_path):
+                rank_dict[model][prop] = None
+                continue
 
             score_dict = pickle.load(open(score_path, "rb"))
             y_pred_pos, y_pred_neg = score_dict['y_pred_pos'], score_dict['y_pred_neg']
@@ -106,6 +112,42 @@ def get_rankings(props, pert_type, models):
     return rank_dict, baseline
 
 
+def error_analysis(rank_dict, baseline, props, out_dir): 
+
+    model = "gnn"
+    baseline_rank = baseline[model]
+
+    G, split_dict = load_data()
+    pos_test_edges = split_dict["test"]["edge"]
+    avg_degs = [(G.degree[node1] + G.degree[node2])/2 for node1, node2 in pos_test_edges]
+
+
+    prop_cmap = {
+        0.1: "#d95f02",
+        0.25: "#7570b3",
+        0.5: "#1b9e77"
+    }
+
+    props = [
+        0.5,
+        # 0.25,
+        0.1
+    ]
+
+    for prop in props:
+        pert_rank = rank_dict[model][prop]
+        prop_change = np.divide(pert_rank, baseline_rank)
+        plt.scatter(avg_degs, prop_change, alpha=0.2, c=prop_cmap[prop], label=prop)
+
+    plt.plot([0, 1600], [1, 1], label="No Change", c="#8dd3c7", linestyle="--")
+    plt.legend()
+    plt.xlabel("Average Degree")
+    plt.ylabel("Average Change in Rank")
+    plt.yscale("log")
+
+    plt.savefig(f"{out_dir}/degree_scatter.png")
+    plt.clf()
+
 
 def plot_average_edge_change(rank_dict, baseline, props, out_dir):
 
@@ -116,30 +158,35 @@ def plot_average_edge_change(rank_dict, baseline, props, out_dir):
             
             baseline_rank = baseline[model]
             pert_rank = rank_dict[model][prop]
+            if pert_rank is None:
+                continue
+            
+            # i.e., number of times worse pertrubed is than baseline on avg
+            prop_change = np.divide(pert_rank, baseline_rank)
 
-            percent_change = np.divide(pert_rank, baseline_rank)
-
-            avg = np.mean(percent_change)
-            std = np.std(percent_change)
+            avg = np.mean(prop_change)
+            std = np.std(prop_change)
 
             if model == "trained_mf":
                 print(prop, avg)
 
-            model_perf[model].append((avg, std))
+            model_perf[model].append(((avg, prop), std))
 
     for model in baseline.keys():
-        avg = [avg for avg, std in model_perf[model]]
-        # err = [std for avg, std in model_perf[model]]
-        plt.plot(props, avg, label=model_name[model], c=model_cmap[model])
-        # plt.errorbar(props, avg, yerr=err, c=model_cmap[model], capsize=5, alpha=0.5)
+        xval = [prop for (avg, prop), std in model_perf[model]]
+        avg = [avg for (avg, prop), std in model_perf[model]]
+        err = [std for (avg, prop), std in model_perf[model]]
+        plt.plot(xval, avg, label=model_name[model], c=model_cmap[model])
+        # plt.errorbar(xval, avg, yerr=err, c=model_cmap[model], capsize=5, alpha=0.5)
 
-    plt.plot([-0.5, 1], [1, 1], label="No Change", alpha=0.3, c="#e7298a", linestyle="--")
+    plt.plot([props[0], props[-1]], [1, 1], label="No Change", alpha=0.3, c="#e7298a", linestyle="--")
     plt.legend()
     plt.xlabel("Perturbation Proportion")
     plt.yscale("log")
     plt.ylabel("Average Change in Rank")
 
     plt.savefig(f"{out_dir}/avg_rank_change.png")
+    plt.clf()
 
 
 
@@ -153,60 +200,58 @@ def plot_hits(props, pert_type, models, out_dir, K=20):
     model2prop_perf = {}
 
     # Get performance for different models
-    for pert in ["add", "remove"]:
-        for prop in props:
-            for model in models:
+    for prop in props:
+        if prop <= 0:
+            pert = "remove"
+            print_prop = -1 * prop
+        else:
+            pert = "add"
+            print_prop = prop
 
-                score_path = f"results/scores/{pert_type}/{pert}/{prop}/{model}_scores.pth"
-                
-                if not os.path.isfile(score_path):
-                    # NOTE: This is needed if some of the scores are still zipped
-                    if os.path.isfile(score_path + ".gz"):
-                        with gzip.open(score_path + ".gz", 'rb') as f_in:
-                            with open(score_path, 'wb') as f_out:
-                                shutil.copyfileobj(f_in, f_out)
-                    else:
-                        print(f"\t=> Skipping:\t{model}\t{pert}\t{prop}")
-                        # print(f"\t\t{score_path}.gz")
-                        continue
+        for model in models:
 
-                score_dict = pickle.load(open(score_path, "rb"))
-
-                start = time.time()
-                hits = evaluator.eval(score_dict)[f'hits@{K}']
-                end = time.time()
-
-                print(f"{model}\t{pert}\t{prop}\t{hits}\t time: {end - start}")
-                for k, v in score_dict.items():
-                    print("\t", model, type(v[0]), v[0])
-                
-
-                if model not in model2prop_perf:
-                    model2prop_perf[model] = []
-                
-                if pert == "remove":
-                    prop_print = prop * -1
+            score_path = f"results/scores/{pert_type}/{pert}/{print_prop}/{model}_scores.pth"
+            
+            if not os.path.isfile(score_path):
+                if os.path.isfile(score_path + ".gz"):
+                    with gzip.open(score_path + ".gz", 'rb') as f_in:
+                        with open(score_path, 'wb') as f_out:
+                            shutil.copyfileobj(f_in, f_out)
                 else:
-                    prop_print = prop
+                    continue
 
-                model2prop_perf[model].append((prop_print, hits))
+            score_dict = pickle.load(open(score_path, "rb"))
+
+            hits = evaluator.eval(score_dict)[f'hits@{K}']
+
+            if model not in model2prop_perf:
+                model2prop_perf[model] = []
+            
+
+            model2prop_perf[model].append(hits)
                 
 
     
     for model in models:
         color = model_cmap[model]
-        data_list = model2prop_perf[model]
-        data_list.sort(key = lambda pair: pair[0])
-        x_val = [pair[0] for pair in data_list]
-        y_val = [pair[1] for pair in data_list]
-        plt.plot(x_val, y_val, label=model_name[model], c=color)
+        y_val = model2prop_perf[model]
+        base = y_val[props.index(0)]
+
+        # Just trying to see MF 0.01, change remove this and fix props soon
+        if len(props) != len(y_val):
+            plt.plot([-0.5, -0.25, -0.1, -0.01, 0, 0.1, 0.25, 0.5, 1], y_val, label=model_name[model], c=color)
+            plt.plot([props[0], props[-1]], [base, base], c=color, linestyle="--", alpha=0.5)
+            continue
+
+        plt.plot(props, y_val, label=model_name[model], c=color)
+        plt.plot([props[0], props[-1]], [base, base], c=color, linestyle="--", alpha=0.5)
 
     # TODO: Maybe we could add dotted lines for unperturbed perfromance for each model
     plt.xlabel("Proportion Perturbed")
-    # plt.xticks([-0.5, -0.25, -0.1, -0.01, 0, 0.01, 0.1, 0.25, 0.5, 1])
     plt.ylabel(f"Hits@{K}")
     plt.legend()
     plt.savefig(out_dir + f"/hits@{K}.png")
+    plt.clf()
 
 
 
